689c305de9365ab64c9ed50786c9f429bb63ca87
irfan6235 <iiiifan6235@gmail.com>
2026-02-28 02:01:06 +0530

    notification

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 
 function App() {
+
+  useEffect(() => {
+    if (window.innerWidth > 1024) {
+      alert(
+        "For the best experience, please view this project on a laptop or desktop device."
+      );
+    }
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
